e86938c3107a6770f3058097d4eca3ec8128d92a
Ifeanyi Dike <ifeanyidike87@gmail.com>
2023-05-07 20:41:35 +0100

    Bundle frontend to backend

---
+++ frontend/src/App.vue
@@ -0,0 +1,112 @@
+<template>
+  <div class="container">
+    <header class="header">
+      <h1>Connect people &amp; spaces</h1>
+    </header>
+    <div v-if="loading">Loading...</div>
+    <div v-else>
+      <ImageList :items="items" class="image-list" />
+      <button>
+        <input
+          @change="handleUploadImage"
+          type="file"
+          class="file"
+          accept="image/png, image/jpeg, image/jpg"
+        />
+        Upload image
+      </button>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import ImageList from './components/ImageList.vue'
+import { ref, onMounted } from 'vue'
+
+const items = ref([])
+const loading = ref(false)
+
+onMounted(async () => {
+  try {
+    loading.value = true
+    const res = await fetch(`http://localhost:5001/images`)
+    items.value = await res.json()
+    loading.value = false
+  } catch (error) {
+    console.log(error)
+  }
+})
+
+const handleUploadImage = async (e) => {
+  try {
+    const file = e.target.files[0]
+    const formData = new FormData()
+    formData.append('img', file)
+    formData.append('itemSize', items.value.length)
+
+    const response = await fetch(`http://localhost:5001/upload-image`, {
+      method: 'POST',
+      body: formData
+    })
+
+    const data = await response.json()
+    items.value.push(data)
+  } catch (error) {
+    console.log(error)
+  }
+}
+</script>
+
+<style scoped>
+
+.container {
+  margin-top: 5px;
+  margin-bottom: 20px;
+}
+.header {
+  font-family: Verdana, Geneva, Tahoma, sans-serif;
+  font-weight: 500;
+  margin: 20px 15px;
+}
+.image-list {
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  justify-items: center;
+}
+
+button {
+  display: block;
+  margin: 0 auto;
+  width: 120px;
+  height: 40px;
+  position: relative;
+  border-radius: 10px;
+  border: 0;
+  cursor: pointer;
+  background-color: #4a52ff;
+  color: #fff;
+  font-size: 1rem;
+}
+
+input[type='file'] {
+  width: 100%;
+  height: 100%;
+  opacity: 0;
+  position: absolute;
+  left: 0;
+  top: 0;
+  cursor: pointer;
+}
+
+@media screen and (max-width: 1024px) {
+  .image-list {
+    grid-template-columns: repeat(2, 1fr);
+  }
+}
+
+@media screen and (max-width: 470px) {
+  .image-list {
+    grid-template-columns: 1fr;
+  }
+}
+</style>
---
+++ frontend/src/components/ImageList.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <div v-for="(item, index) in props.items" :key="index">
+      <ImageDetail :item="item" />
+    </div>
+  </div>
+</template>
+
+<script setup>
+import ImageDetail from '../components/ImageDetail.vue'
+const props = defineProps(['items'])
+</script>
+
+<style scoped></style>
